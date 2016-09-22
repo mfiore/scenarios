4c50373cefdb53cc925856d51133971280dfefc4
Michelangelo Fiore <mfiore@bagatelle.laas.fr>
2016-09-22 17:20:01 +0200

    fixed fill

--- demo_observer/src/actions/fill.cpp
+++ demo_observer/src/actions/fill.cpp
@@ -11,45 +11,58 @@ bool Fill::checkPreconditions(StringMap parameters) {
 
 	if (!checkParameterPresence(parameters)) return false;
 
+	string agent=parameters.at("main_agent");
+	string object=parameters.at("main_object");
+	string target=parameters.at("target");
 	
 	situation_assessment_msgs::Fact has_query;
 	has_query.model=robot_name_;
-	has_query.subject=parameters.at("main_agent");
+	has_query.subject=agent;
 	has_query.predicate.push_back("has");
-	has_query.predicate.push_back(parameters.at("main_object"));
+	has_query.value.push_back(object);
 
-	bool has_object=std::stoi(queryDatabase(has_query));
+	ROS_INFO("%s %s %s ",robot_name_.c_str(),agent.c_str(),object.c_str());
 
+	bool has_object=queryDatabase(has_query)!="";
+
+	// ROS_INFO("Has object is %d",has_object);
 	if (!has_object) return false;
 
 
 	situation_assessment_msgs::Fact capacity_query;
 	capacity_query.model=robot_name_;
-	capacity_query.subject=parameters.at("main_object");
+	capacity_query.subject=object;
 	capacity_query.predicate.push_back("capacity");
 
 	int object_capacity=std::stoi(queryDatabase(capacity_query));
 
+	// ROS_INFO("capacity is %d",object_capacity);
 	if (object_capacity==0) return false;
+	return true;
+	// situation_assessment_msgs::Fact agent_isAt_query,target_isAt_query;
+	// agent_isAt_query.model=robot_name_;
+	// agent_isAt_query.subject=parameters.at("main_agent");
+	// agent_isAt_query.predicate.push_back("at");
 
-	situation_assessment_msgs::Fact agent_isAt_query,target_isAt_query;
-	agent_isAt_query.model=robot_name_;
-	agent_isAt_query.subject=parameters.at("main_agent");
-	agent_isAt_query.predicate.push_back("at");
+	// string agent_location=queryDatabase(agent_isAt_query);
 
-	string agent_location=queryDatabase(agent_isAt_query);
+	// target_isAt_query=agent_isAt_query;
+	// target_isAt_query.subject=parameters.at("target");
 
-	target_isAt_query=agent_isAt_query;
-	target_isAt_query.subject=parameters.at("target");
+	// string target_location=queryDatabase(target_isAt_query);
 
-	string target_location=queryDatabase(target_isAt_query);
-
-	return agent_location==target_location;
+	// return agent_location==target_location;
 
 }
 void Fill::setPostconditions(StringMap parameters) {
+	if (!checkParameterPresence(parameters)) {
+		ROS_INFO("FILL - not all parameters are present");
+		return;
+	}
+
+
 	string target=parameters.at("target");
-	string object=parameters.at("object");
+	string object=parameters.at("main_object");
 
 	situation_assessment_msgs::Fact target_capacity,object_capacity;
 	target_capacity.model=robot_name_;
